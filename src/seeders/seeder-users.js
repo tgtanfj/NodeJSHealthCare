6859c8e9e4e555a6d9a5bea5490cd2bd999c92a4
TrungTan <trungtan14@gmail.com>
2022-11-14 12:25:09 +0700

    first commit master

---
+++ src/seeders/seeder-users.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Users', [{
+      email: 'admin@gmail.com',
+      password: '123456',
+      firstName: 'Le Trung',
+      lastName: 'Tan',
+      address: 'VietNam',
+      gender: 1,
+      typeRole: 'ROLE',
+      keyRole: 'R1', 
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
