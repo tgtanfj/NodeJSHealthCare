7f60e0c75947c9c11195055eec1ff3f69fe05805
trungtan <trungtan14@gmail.com>
2022-12-13 00:15:06 +0700

    commit 69 done

--- src/route/web.js
+++ src/route/web.js
@@ -9,7 +9,6 @@ let initWebRoutes = (app) => {
     router.get('/', homeController.getHomePage);
     router.get('/about', homeController.getAboutPage);
     router.get('/crud', homeController.getCRUD);
-
     router.post('/post-crud', homeController.postCRUD);
     router.get('/get-crud', homeController.displayGetCRUD)
     router.get('/edit-crud', homeController.getEditCRUD)
@@ -21,13 +20,12 @@ let initWebRoutes = (app) => {
     router.post('/api/create-new-user', userController.handleCreateNewUser)
     router.put('/api/edit-user', userController.handleEditUser)
     router.delete('/api/delete-user', userController.handleDeleteUser)
-
     router.get('/api/getallcode', userController.getAllCode)
 
     router.get('/api/top-doctor-home', doctorController.getTopDoctorHome)
-
     router.get('/api/get-all-doctors', doctorController.getAllDoctors)
     router.post('/api/save-info-doctors', doctorController.postInfoDoctors)
+    router.get('/api/get-detail-doctor-by-id', doctorController.getDetailDoctorById)
 
     return app.use("/", router);
 }
--- src/services/doctorSevices.js
+++ src/services/doctorSevices.js
@@ -1,3 +1,4 @@
+import e from 'cors'
 import db from '../models/index'
 
 let getTopDoctorHome = (limitInput) => {
@@ -49,9 +50,9 @@ let getAllDoctors = () => {
 }
 
 let saveDetailInfoDoctor = (inputData) => {
-    return new Promise(async(resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         try {
-            if(!inputData.doctorId || !inputData.contentHTML || !inputData.contentMarkdown) {
+            if (!inputData.doctorId || !inputData.contentHTML || !inputData.contentMarkdown) {
                 resolve({
                     errCode: 1,
                     errMessage: 'Missing parameter!'
@@ -74,8 +75,54 @@ let saveDetailInfoDoctor = (inputData) => {
     })
 }
 
+let getDetailDoctorById = (inputId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!inputId) {
+                resolve({
+                    errCode: 1,
+                    errMessage: "Missing required parameter!"
+                })
+            } else {
+                let data = await db.User.findOne({
+                    where: {
+                        id: inputId
+                    },
+                    attributes: {
+                        exclude: ['password']
+                    },
+                    // include: lay thong tin cua User kem theo thong tin user do trong bang Markdown
+                    include: [
+                        {
+                            model: db.Markdown,
+                            attributes: ['description', 'contentHTML', 'contentMarkDown']
+                        },
+                        { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
+                    ],
+                    raw: false,
+                    nest: true
+                })
+
+                if(data && data.image) {
+                    data.image = new Buffer(data.image, 'base64').toString('binary')
+                }
+
+                if(!data) data = {}
+
+                resolve({
+                    errCode: 0,
+                    data: data
+                })
+            }
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
     saveDetailInfoDoctor: saveDetailInfoDoctor,
+    getDetailDoctorById: getDetailDoctorById,
 }
